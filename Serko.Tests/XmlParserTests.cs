30bfb2e6c358a46c165fb6fbdad10901b3e1e94f
Ankur Sheel <ankur.sheel@gmail.com>
2019-07-17 15:37:43 +1200

    add test to see if valid xml with email works

--- Serko.Tests/XmlParserTests.cs
+++ Serko.Tests/XmlParserTests.cs
@@ -50,6 +50,41 @@ Ivan
         }
 
         [Fact]
+        public void ExtractXml_ValidFullEmailTextWithNoEmail_ValidEmailDataModel()
+        {
+            const string EmailText = @"
+Hi Yvaine,
+Please create an expense claim for the below. Relevant details are marked up as requested…
+
+<expense><cost_centre>DEV002</cost_centre>
+    <total>1024.01</total><payment_method>personal card</payment_method>
+</expense>
+
+From: Ivan Castle
+Sent: Friday, 16 February 2018 10:32 AM
+Subject: test
+
+Hi Antoine,
+
+Please create a reservation at the <vendor>Viaduct Steakhouse</vendor> our 
+<description>development team’s project end celebration dinner</description> on
+<date>Tuesday 27 April 2017</date>. We expect to arrive around
+7.15pm. Approximately 12 people but I’ll confirm exact numbers closer to the day.
+
+Regards,
+Ivan
+";
+
+            var emailData = _xmlParser.ExtractXml(EmailText);
+            Assert.Equal("DEV002", emailData.Expense.CostCenter);
+            Assert.Equal(1024.01M, emailData.Expense.Total);
+            Assert.Equal("personal card", emailData.Expense.PaymentMethod);
+            Assert.Equal("Viaduct Steakhouse", emailData.Vendor);
+            Assert.Equal("development team’s project end celebration dinner", emailData.Description);
+            Assert.Equal("Tuesday 27 April 2017", emailData.Date);
+        }
+
+        [Fact]
         public void ExtractXml_ValidExpenseText_ValidExpenseModel()
         {
             const string EmailText = @"
--- Serko/Models/EmailData.cs
+++ Serko/Models/EmailData.cs
@@ -8,11 +8,14 @@ namespace Serko.Models
         [XmlElement("expense")]
         public Expense Expense { get; set; }
 
+        [XmlElement("vendor")]
         public string Vendor { get; set; }
 
+        [XmlElement("description")]
         public string Description { get; set; }
 
         //ToDo: Should this be a DateTime or something similar?
+        [XmlElement("date")]
         public string Date { get; set; }
     }
 }
