439811681652efd89b779ac7804e3abe1c38eab7
Ankur Sheel <ankur.sheel@gmail.com>
2019-07-18 12:02:01 +1200

    combine integration test

--- Serko.Tests/Integration/EmailDataControllerTests.cs
+++ Serko.Tests/Integration/EmailDataControllerTests.cs
@@ -34,7 +34,7 @@ namespace Serko.Tests.Integration
         }
 
         [Fact]
-        public async Task Post_ValidFullEmailText_OkResponseAndExtractedData()
+        public async Task Post_ValidFullEmailText_OkResponse()
         {
             const string EmailText = @"
 Hi Yvaine,
@@ -76,48 +76,12 @@ Ivan
             Assert.Equal("Viaduct Steakhouse", emailDataResponse.ExtractedData.Vendor);
             Assert.Equal("development team’s project end celebration dinner", emailDataResponse.ExtractedData.Description);
             Assert.Equal("Tuesday 27 April 2017", emailDataResponse.ExtractedData.Date);
-        }
-
-        [Fact]
-        public async Task Post_ValidFullEmailText_OkResponseAndCalculatedData()
-        {
-            const string EmailText = @"
-Hi Yvaine,
-Please create an expense claim for the below. Relevant details are marked up as requested…
-
-<expense><cost_centre>DEV002</cost_centre>
-    <total>1024.01</total><payment_method>personal card</payment_method>
-</expense>
-
-From: Ivan Castle
-Sent: Friday, 16 February 2018 10:32 AM
-To: Antoine Lloyd <Antoine.Lloyd@example.com>
-Subject: test
-
-Hi Antoine,
-
-Please create a reservation at the <vendor>Viaduct Steakhouse</vendor> our 
-<description>development team’s project end celebration dinner</description> on
-<date>Tuesday 27 April 2017</date>. We expect to arrive around
-7.15pm. Approximately 12 people but I’ll confirm exact numbers closer to the day.
-
-Regards,
-Ivan
-";
-            var input = new PostEmailDataRequest { Data = EmailText };
-            var content = new StringContent(JsonConvert.SerializeObject(input), Encoding.UTF8, "application/json");
-            var response = await _client.PostAsync("api/email", content);
-
-            response.EnsureSuccessStatusCode();
-
-            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
-            var jsonFromPostResponse = await response.Content.ReadAsStringAsync();
-
-            var emailDataResponse = JsonConvert.DeserializeObject<PostEmailDataResponse>(jsonFromPostResponse);
 
             // Data obtained from http://gstcal.co.nz/default.aspx
+            Assert.Equal(1024.01M, emailDataResponse.Totals.TotalIncludingGst);
             Assert.Equal(133.57M, emailDataResponse.Totals.Gst);
             Assert.Equal(890.44M, emailDataResponse.Totals.TotalExcludingGst);
+
         }
 
         [Fact]
